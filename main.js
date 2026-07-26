9a6f703e49063372425158ad232d7feb0a31d102
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-07-26 13:49:21 +0300

    add partioal js logic

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("Hello, world!!!!!!!!!!!!")
+
+const choices = ["rock", "paper", "scissors"]
+
+function randomer()
+{
+    return(Math.random());
+}
+
+function getComputerChoice()
+{
+
+
+}
+
+console.log(randomer());
