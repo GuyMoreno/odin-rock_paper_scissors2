216031550c2228c9264cf7730d6a6c814f6fef8d
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-07-29 13:41:25 +0300

    -start implementing playRound function

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 console.log("Hello, world!!!!!!!!!!!!")
 
+const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function randomNumberGenerator()
 {
-    return(Math.floor(Math.random()*3));
+return(Math.floor(Math.random()*3));
 }
 
 function getComputerChoice()
 {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[randomNumberGenerator()];
+return choices[randomNumberGenerator()];
 }
 
 function getHumanChoice()
 {
-    const choice = prompt("Enter your choice: rock, paper, or scissors")
-    return choice;
-}
+let choice = prompt("Enter your choice: rock, paper, or scissors");
 
-let humanScore = 0;
-let computerScore = 0;
+while (!choices.includes(choice.toLowerCase()))
+    {
+    choice = prompt("Invalid choice. Please enter rock, paper, or scissors");
+    }
+
+return choice.toLowerCase();
+}
 
 console.log(getComputerChoice())
 console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return;
+  }
+  const beats = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper"
+};
+
+if (beats[humanChoice] === computerChoice) {
+  console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  humanScore++;
+} 
+}
 
